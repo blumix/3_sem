8fe810ee8e536a1e8276e16338f84e133f488009
Mikhail <zigawick@gmail.com>
2018-03-11 17:14:30 +0300

    utf test

--- hadoop/hw1/src/main/java/WordCountJob.java
+++ hadoop/hw1/src/main/java/WordCountJob.java
@@ -25,7 +25,7 @@ public class WordCountJob extends Configured implements Tool {
             // split by space symbols (space, tab, ...)
 //            Set<String> uniqueWords = new HashSet<>(Arrays.asList(line.split("[^\\p{L}+]")));
 //            for(String word: uniqueWords)
-            context.write(new Text(value), one);
+            context.write(value, one);
         }
     }
 
@@ -47,8 +47,8 @@ public class WordCountJob extends Configured implements Tool {
         job.setJarByClass(WordCountJob.class);
         job.setJobName(WordCountJob.class.getCanonicalName());
 
-        job.setInputFormatClass(DocCollectionInputFormat.class);
-        DocCollectionInputFormat.addInputPath(job, new Path(input));
+        job.setInputFormatClass(UtfTestInputFormat.class);
+        UtfTestInputFormat.addInputPath(job, new Path(input));
         FileOutputFormat.setOutputPath(job, new Path(output));
 
         job.setMapperClass(WordCountMapper.class);
