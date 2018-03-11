469de3a05b2798eec4d83fbfc891dfc9ca60a12d
Mikhail <zigawick@gmail.com>
2018-03-11 12:41:03 +0300

    launch fix

--- hadoop/hw1/lauch.sh
+++ hadoop/hw1/lauch.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-hadoop dfs -rm -r /out
+hadoop dfs -rm -r out
 gradle jar
-hadoop jar build/libs/hw1.jar WordCountJob /11e6c777-38e4-4584-86f5-2e50c4144854.pkz /out
+hadoop jar build/libs/hw1.jar WordCountJob /data/hw1/*.pkz out
