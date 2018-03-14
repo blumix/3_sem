f04d5ccabcc20be8b0f8058363683668bdac8273
Mikhail <zigawick@gmail.com>
2018-03-14 20:12:25 +0300

    fix go xgboost

--- dm/hw1/xgboost_tree_regressor.py
+++ dm/hw1/xgboost_tree_regressor.py
@@ -17,7 +17,7 @@ class SplitFinder:
         self.feature_num = None
         self.feature_val = None
         self.splitting_number = None
-        self.number_of_elements = len(self.s)
+        self.number_of_elements = len(self.g)
         self.sum_s = np.sum(self.s)
         self.sum_g = np.sum(self.g)
         self.values_list = range(1, self.x.shape[0])
@@ -37,43 +37,43 @@ class SplitFinder:
 
     def find_best_split_feature(self, num):
         sorted_g = self.g[self.argsort_x[:, num]]
-        sorted_s = self.s[self.argsort_x[:, num]]
         sorted_x = self.x[self.argsort_x[:, num], num]
 
-        Gr = sorted_g[:self.values_list[0]].sum()
-        Gl = sorted_g[self.values_list[0]:].sum()
+        self.cur_Gr = sorted_g[:self.values_list[0]].sum()
+        self.cur_Gl = sorted_g[self.values_list[0]:].sum()
 
-        Sr = sorted_s[:self.values_list[0]].sum()
-        Sl = sorted_s[self.values_list[0]:].sum()
+        self.cur_Sr = self.values_list[0] * self.s
+        self.cur_Sl = (self.number_of_elements - self.values_list[0]) * self.s
         for val_num in self.values_list:
             if sorted_x[val_num] == sorted_x[val_num - 1]:
-                self.update_sum(Gl, Gr, Sl, Sr, sorted_g, sorted_s, val_num)
+                self.update_sum(sorted_g, val_num)
                 continue
 
-            gain = self.calculate_gain(Gl, Gr, Sl, Sr)
+            gain = self.calculate_gain()
 
-            self.update_sum(Gl, Gr, Sl, Sr, sorted_g, sorted_s, val_num)
+            self.update_sum(sorted_g, val_num)
             if not self.max_gain or self.max_gain < gain:
                 self.feature_num = num
                 self.feature_val = (sorted_x[val_num - 1] + sorted_x[val_num]) / 2.
                 self.splitting_number = val_num
                 self.max_gain = gain
-                self.Gr = Gr
-                self.Gl = Gl
-                self.Sr = Sr
-                self.Sl = Sl
+                self.Gr = self.cur_Gr
+                self.Gl = self.cur_Gl
+                self.Sr = self.cur_Sr
+                self.Sl = self.cur_Sl
 
-    def calculate_gain(self, Gl, Gr, Sl, Sr):
-        gain = Gl * Gl / (Sl + self.lambda_v) + Gr * Gr / (Sr + self.lambda_v) - (Gl + Gr) * (Gl + Gr) / (
-                Sl + Sr + self.lambda_v) - self.gamma
+    def calculate_gain(self):
+        gain = self.cur_Gl * self.cur_Gl / (self.cur_Sl + self.lambda_v) + self.cur_Gr * self.cur_Gr / (
+                self.cur_Sr + self.lambda_v) - (self.cur_Gl + self.cur_Gr) * (self.cur_Gl + self.cur_Gr) / (
+                       self.cur_Sl + self.cur_Sr + self.lambda_v) - self.gamma
         return gain
 
-    def update_sum(self, Gl, Gr, Sl, Sr, sorted_g, sorted_s, val_num):
-        Gr += sorted_g[val_num]
-        Sr += sorted_s[val_num]
+    def update_sum(self, sorted_g, val_num):
+        self.cur_Gr += sorted_g[val_num]
+        self.cur_Sr += self.s
 
-        Gl -= sorted_g[val_num]
-        Sl -= sorted_s[val_num]
+        self.cur_Gl -= sorted_g[val_num]
+        self.cur_Sl -= self.s
 
 
 class MyXGBoostTreeRegressor:
@@ -90,15 +90,16 @@ class MyXGBoostTreeRegressor:
 
     def __fit_node(self, x, g, s, node_id, depth):
         if self.max_depth is not None and depth == self.max_depth:
-            self.tree[node_id] = (self.LEAF_TYPE, - g.sum() / (s.sum() + self.lambda_v))
+            self.tree[node_id] = (self.LEAF_TYPE, - g.sum() / (s * len(g) + self.lambda_v))
             return
         if self.min_samples_split is not None and x.shape[0] < self.min_samples_split:
-            self.tree[node_id] = (self.LEAF_TYPE, - g.sum() / (s.sum() + self.lambda_v))
+            self.tree[node_id] = (self.LEAF_TYPE, - g.sum() / (s * len(g) + self.lambda_v))
             return
 
         sf = SplitFinder(x, g, s, lambda_v=self.lambda_v, gamma=self.gamma)
-        if sf.max_gain is None:
-            self.tree[node_id] = (self.LEAF_TYPE, - g.sum() / (s.sum() + self.lambda_v))
+        sf.find_best_split()
+        if sf.max_gain is None or sf.max_gain < 0:
+            self.tree[node_id] = (self.LEAF_TYPE, - g.sum() / (s * len(g) + self.lambda_v))
             return
 
         sorted_field = sf.argsort_x[:, sf.feature_num]
@@ -106,13 +107,11 @@ class MyXGBoostTreeRegressor:
 
         x_l = x[sorted_field][:sf.splitting_number]
         g_l = g[sorted_field][:sf.splitting_number]
-        s_l = s[sorted_field][:sf.splitting_number]
-        self.__fit_node(x_l, g_l, s_l, 2 * node_id + 1, depth + 1)
+        self.__fit_node(x_l, g_l, s, 2 * node_id + 1, depth + 1)
 
         x_r = x[sorted_field][sf.splitting_number:]
         g_r = g[sorted_field][sf.splitting_number:]
-        s_r = s[sorted_field][sf.splitting_number:]
-        self.__fit_node(x_r, g_r, s_r, 2 * node_id + 2, depth + 1)
+        self.__fit_node(x_r, g_r, s, 2 * node_id + 2, depth + 1)
 
     def fit(self, x, g, s):
         self.__fit_node(x, g, s, 0, 0)
@@ -170,14 +169,14 @@ class XGB:
 
     def fit_tree(self, X, y):
         g = -2 * (y - self.h)
-        s = 2. * np.ones(self.h.shape)
+        s = 2.
         a_i = MyXGBoostTreeRegressor(max_depth=self.max_depth)
         a_i.fit(X, g, s)
         # res = a_i.predict(X)
         b = self.learning_rate
         self.b.append(b)
         self.trees.append(a_i)
-        self.h = self.h = b * a_i.predict(X)
+        self.h = self.h - b * a_i.predict(X)
         self.tree_num += 1
 
     def predict(self, X):
@@ -189,7 +188,7 @@ class XGB:
     def staged_predict(self, X):
         single_res = np.ones(X.shape[0]) * self.med
         ret = [np.copy(single_res)]
-        for i, tree in enumerate (self.trees):
+        for i, tree in enumerate(self.trees):
             single_res += tree.predict(X) * self.learning_rate * self.b[i]
             temp = np.copy(single_res)
             ret.append(temp)
@@ -217,14 +216,14 @@ def load_data_2():
 
 if __name__ == '__main__':
 
-    est_num = 6
+    est_num = 30
     X_train, X_test, y_train, y_test = load_data_1()
     l1_err_train = []
     l1_err_test = []
     l1_test_err_train = []
     l1_test_err_test = []
 
-    boo = XGB(n_estimators=est_num, verbose=True, learning_rate=0.1)
+    boo = XGB(n_estimators=est_num, verbose=True, learning_rate=0.1, max_depth=40)
     boo.fit(X_train, y_train)
     my_res_train = boo.staged_predict(X_train)
     my_res_test = boo.staged_predict(X_test)
