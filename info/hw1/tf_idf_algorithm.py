00f4217a2d7aedf3a1da4495e8b747bec40183a7
Mikhail <zigawick@gmail.com>
2018-04-12 10:35:33 +0300

    refactor hw1

--- info/hw1/tf_idf_algorithm.py
+++ info/hw1/tf_idf_algorithm.py
@@ -23,53 +23,39 @@ def get_cosine_sim(query, docs, extractor):
     return index_sparse[query_bow]
 
 
-# def get_tf_idf(query, docs, extractor):
-#     stopwords_norm = DSR.clear_text(' '.join(stopwords.words('russian')))
-#     gen_dict = gensim.corpora.Dictionary(
-#         [[w for w in extractor(doc) if w not in stopwords_norm] for doc in read_docs()], prune_at=None)
-#     gen_dict.save("gen_dict.dict")
-#     raw_corpus = [gen_dict.doc2bow([w for w in extractor(doc) if w not in stopwords_norm]) for doc in read_docs()]
-#     # gensim.corpora.MmCorpus.serialize('corpa.mm', raw_corpus)  # store to disk
-#     dictionary = gensim.corpora.Dictionary.load('gen_dict.dict')
-#     corpus = gensim.corpora.MmCorpus('corpa.mm')
-#     # tfidf_model = gensim.models.TfidfModel(dictionary=dictionary)#, wglobal=wglob, wlocal=wloc)
-#     # index_sparse = gensim.similarities.SparseMatrixSimilarity(corpus, num_features=corpus.num_terms)
-#     # tfidf_model.save("tf_idf.model")
-#     # index_sparse.save("index_sparse.matrix")
-#     #
-#     tfidf_model = gensim.models.TfidfModel.load("tf_idf.model")
-#     # index_sparse = gensim.similarities.SparseMatrixSimilarity.load("index_sparse.matrix")
-#     tf_idf_corpa = tfidf_model[corpus]
-#
-#     map_docs_to_nums = pickle.load(open("map_docs_to_nums.pickle", "rb"))
-#
-#     w_queries = read_queries_to_scan()
-#
-#     res_file = open("sub_tf-idf.csv", "w")
-#     res_file.write('QueryId,DocumentId\n')
-#
-#     q_rates = defaultdict(list)
-#
-#     for i, q in enumerate(sorted(read_queries().items())):
-#         query_bow = dictionary.doc2bow(q[1])
-#
-#         for id_num in w_queries[q[0]]:
-#             if id_num not in map_docs_to_nums:
-#                 print(f"skipped {id_num}")
-#                 continue
-#             ti_doc = tf_idf_corpa[map_docs_to_nums.index(id_num)]
-#             q_rates[q[0]].append((id_num, get_rate(ti_doc, query_bow)))
-#
-#             # d_i = 0
-#             # st = np.argsort(-index_sparse[query_bow])
-#             # for index in st:
-#             #     if map_docs_to_nums[index] in w_queries[q[0]]:
-#             #         res_file.write(f"{q[0]},{map_docs_to_nums[index]}\n")
-#             #         d_i += 1
-#             #     if d_i == 10:
-#             #         break
-#         print(f"{i} docs.")
-#     pickle.dump(q_rates, open("q_rates.pickle", "wb"))
+def wglob(D, total_docs):
+    return np.log(1 + D / total_docs)
+
+
+def wloc(f):
+    return 1 + np.log(f)
+
+
+def get_rate(ti_doc, query_bow):
+    res = 0
+    q_ids = [q[0] for q in query_bow]
+    for id, value in ti_doc:
+        if id in q_ids:
+            res += value
+    return res
+
+
+def get_tf_idf(query, docs, extractor):
+    stopwords_norm = DSR.clear_text(' '.join(stopwords.words('russian')))
+    dictionary = gensim.corpora.Dictionary(
+        [[w for w in extractor(doc) if w not in stopwords_norm] for doc in docs], prune_at=None)
+
+    raw_corpus = [dictionary.doc2bow([w for w in extractor(doc) if w not in stopwords_norm]) for doc in docs]
+    gensim.corpora.MmCorpus.serialize(f'temp/corpa_{query[0]}.mm', raw_corpus)
+    corpus = gensim.corpora.MmCorpus(f'temp/corpa_{query[0]}.mm')
+    tfidf_model = gensim.models.TfidfModel(dictionary=dictionary)  # , wglobal=wglob, wlocal=wloc)
+    tf_idf_corpa = tfidf_model[corpus]
+
+    query_bow = dictionary.doc2bow(query[1])
+
+    scores = []
+    for ti_doc in tf_idf_corpa:
+        scores.append(get_rate(ti_doc, query_bow))
 
 
 def one_query_job(query):
