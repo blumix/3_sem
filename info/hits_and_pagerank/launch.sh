a0d440e1e889140dec3d2d5b96dd10470ebaa938
Mikhail <zigawick@gmail.com>
2018-04-22 13:30:57 +0300

    hits and rank

---
+++ info/hits_and_pagerank/launch.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+hdfs dfs -rm -r hits_graph
+git fetch
+git rebase
+gradle jar
+hadoop jar build/libs/hits_and_pagerank.jar GraphBuilder /data/infopoisk/hits_pagerank/docs-000.txt hits_graph
