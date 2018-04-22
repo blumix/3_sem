880a874e3e76da664e024b2e2adc5ae513000069
Mikhail <zigawick@gmail.com>
2018-04-22 18:16:31 +0300

    hits and page

--- info/hits_and_pagerank/launch.sh
+++ info/hits_and_pagerank/launch.sh
@@ -3,5 +3,7 @@
 hdfs dfs -rm -r hits_graph&
 git fetch
 git rebase
+export LIBJARS=/home/m.belozerov/3_sem/info/hits_and_pagerank/jsoup-1.11.3.jar
+export HADOOP_CLASSPATH=/home/m.belozerov/3_sem/info/hits_and_pagerank/jsoup-1.11.3.jar
 gradle jar
-hadoop jar build/libs/hits_and_pagerank.jar GraphBuilder /data/infopoisk/hits_pagerank/docs-000.txt hits_graph
+hadoop jar build/libs/hits_and_pagerank.jar GraphBuilder -libjars ${LIBJARS} /data/infopoisk/hits_pagerank/docs-000.txt hits_graph
