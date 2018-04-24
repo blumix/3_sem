78439ec434271707e0f00a950c020f7bb028f3bc
Mikhail <zigawick@gmail.com>
2018-04-25 00:04:54 +0300

    hits

---
+++ info/hits_and_pagerank/get_top.py
@@ -0,0 +1,28 @@
+f_a = open ("a.txt")
+f_b = open ("b.txt")
+f_urls = open ("urls.txt")
+urls = {}
+for u in f_urls.readlines ():
+    u = u.strip ().split ('\t')
+    urls[u[0]] = u[1]
+
+a = {}
+for l in f_a.readlines ():
+    l = l.strip ().split ('\t')
+    a[l[0]] = float (l[1].lower ())
+
+b = {}
+for l in f_b.readlines ():
+    l = l.strip ().split ('\t')
+    b[l[0]] = float (l[1].lower ())
+
+sorted_a = sorted(a.items(), key=lambda x:x[1], reversed=True)
+sorted_b = sorted(b.items(), key=lambda x:x[1], reversed=True)
+
+print ("Top A:")
+for s in sorted_a[:30]:
+    print (urls[s[0]])
+
+print ("Top B:")
+for s in sorted_b[:30]:
+    print (urls[s[0]])
